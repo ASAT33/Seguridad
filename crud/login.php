7ae0623da7d97584d4ef6067c8f8a62cbc0dfcef
asat33 <abidielsoto@gmail.com>
2024-07-11 22:39:13 -0500

    final

--- crud/login.php
+++ crud/login.php
@@ -3,22 +3,23 @@ session_start();
 require_once('../class/functions.php');
 require_once('../crud/email_functions.php');
 
+$logFile = '../class/login_logs.log';
 
 define('MAX_FAILED_ATTEMPTS', 3);
 
 define('ATTEMPT_WINDOW', 900); 
 
+$obj_funciones = new funciones();
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
-
-
+    $client_ip = $_SERVER['REMOTE_ADDR'];
     if (!isset($_COOKIE['failed_attempts_' . $username])) {
         setcookie('failed_attempts_' . $username, 0, time() + ATTEMPT_WINDOW, "/");
         setcookie('first_attempt_time_' . $username, time(), time() + ATTEMPT_WINDOW, "/");
-        $_COOKIE['failed_attempts_' . $username] = 0;
-        $_COOKIE['first_attempt_time_' . $username] = time();
     }
+
     $failed_attempts = (int)$_COOKIE['failed_attempts_' . $username];
     $first_attempt_time = (int)$_COOKIE['first_attempt_time_' . $username];
 
@@ -26,7 +27,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $time_since_first_attempt = time() - $first_attempt_time;
 
         if ($time_since_first_attempt < ATTEMPT_WINDOW) {
-            header("Location: ../index.php?error=limite_superado"); 
+            logAttempt($logFile, $username, $client_ip, "Intento fallido - Límite de intentos superado");
+            header("Location: ../index.php?error=limite_superado");
             exit();
         } else {
             setcookie('failed_attempts_' . $username, 0, time() + ATTEMPT_WINDOW, "/");
@@ -35,20 +37,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $first_attempt_time = time();
         }
     }
-
-    $obj_funciones = new funciones();
     $login_success = $obj_funciones->verificar_login($username, $password);
 
     if ($login_success) {
-
         setcookie('failed_attempts_' . $username, 0, time() + ATTEMPT_WINDOW, "/");
-
+        logAttempt($logFile, $username, $client_ip, "Inicio de sesión exitoso");
         $verification_code = substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+{}[]|\/?><'), 0, 6);
-
         $_SESSION['verification_code'] = $verification_code;
         $_SESSION['username'] = $username;
         $user_email = $username;
-
         if (sendVerificationCode($user_email, $verification_code)) {
             header("Location: ../verificar_codigo.php");
             exit();
@@ -57,18 +54,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             exit();
         }
     } else {
-
         $failed_attempts++;
         setcookie('failed_attempts_' . $username, $failed_attempts, time() + ATTEMPT_WINDOW, "/");
+        
         if ($failed_attempts == 1) {
             setcookie('first_attempt_time_' . $username, time(), time() + ATTEMPT_WINDOW, "/");
         }
-
-        header("Location: ../index.php?error=1");
+        logAttempt($logFile, $username, $client_ip, "Intento fallido - Credenciales incorrectas");
+        header("Location: ../index.php?error=1"); 
         exit();
     }
 } else {
     header("Location: ../index.php");
     exit();
+}
+
+function logAttempt($logFile, $username, $client_ip, $message) {
+    $logData = "[" . date('Y-m-d H:i:s') . "] IP: {$client_ip} - Username: {$username} - {$message}" . PHP_EOL;
+    file_put_contents($logFile, $logData, FILE_APPEND);
 }
 ?>
